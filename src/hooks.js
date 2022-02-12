70b317e892818c05585f580326d6199a44439cd0
edde746 <86283021+edde746@users.noreply.github.com>
2022-02-12 17:12:31 +0100

    Parse Cookies & Disconnect Redis

--- src/hooks.js
+++ src/hooks.js
@@ -1,9 +1,18 @@
 import { parse } from "cookie";
 import { getSession as sessionFromCookies } from "$lib/utils";
+import { disconnect } from "$lib/redis";
+
+export const handle = async ({ event, resolve }) => {
+  const cookies = parse(event.request.headers.get("cookie") || "");
+  event.locals.session = await sessionFromCookies(cookies.session);
+
+  const response = await resolve(event);
+  await disconnect();
+  return response;
+};
 
 export const getSession = async ({ request }) => {
   const cookies = parse(request.headers.get("cookie") || "");
   const session = await sessionFromCookies(cookies.session);
-
   return session ? session : false;
 };
